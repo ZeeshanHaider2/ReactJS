337a75f13f1f90abc2fd4ec57122a6b4a5ac90af
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2022-09-07 23:50:43 +0200

    request to spotify api

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -51,9 +51,31 @@ const Spotify = {
 
           }))
        })
+    },
+
+    savePlaylist(name,trackUris){
+     if(!name || !trackUris.length){
+        return;
+     }
+     const accessToken = Spotify.getAccessToken();
+     const headers =  {Authorization: `Bearer ${accessToken}`}
+     let userId;
+
+     return fetch('https://api.spotify.com/v1/me', {headers:headers}
+     ).then(response => response.json()
+     ).then(jsonResponse => {
+        userId = jsonResponse.id;
+        return fetch(`https://:api.spotify.com//v1/users/${userId}/playlists`,
+        {
+            headers : headers,
+            method: 'POST',
+            body: JSON.stringify ({name:name})
+        });
+
+     })
     }
 
 }
-
+///v1/users/{user_id}/playlists/{playlist_id}/tracks
 
 export default Spotify;
